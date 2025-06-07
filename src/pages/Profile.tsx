264da16ec8f028f9f311202bed654d66589b1f11
Asem Asfandiarova <assemsim@gmail.com>
2025-06-07 23:08:11 +0600

    feat: profile page navigations

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ProfileComponent from '@/components/Profile-components/ProfileComponent';
+import RedirectUnauthorizedUser from '@/components/Profile-components/RedirectUnauthorizedUser';
 
 function Profile() {
-  return <ProfileComponent />;
+  return (
+    <>
+      <RedirectUnauthorizedUser />
+      <ProfileComponent />
+    </>
+  );
 }
 
 export default Profile;
